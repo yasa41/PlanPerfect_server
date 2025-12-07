b0558ce77375aaff9baf7f7be995158607f79d2b
Yasaswini <yasa2005.04@gmail.com>
2025-12-07 13:37:44 +0530

    gmail smtp changes

--- config/email.js
+++ config/email.js
@@ -1,7 +1,9 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
